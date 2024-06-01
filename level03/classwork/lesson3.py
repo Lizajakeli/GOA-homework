713c8846c5dba9109a304ed82821717cf7543971
lizajakeli <jakeliliza@gmail.com>
2024-06-01 12:16:47 +0400

    third commit

---
+++ level03/classwork/lesson3.py
@@ -0,0 +1,28 @@
+name = "liza"
+
+last_name = "jakeli"
+
+age = (12)
+
+hobby = "dance"
+
+country = "Georgia"
+
+print(name)
+print(last_name)
+print(age)
+print(hobby)
+print(country)
+
+
+print(56 + 24)
+print(90 + 45)
+print(34 - 23)
+print(123 - 46)
+print(12 * 3)
+print(25 * 8)
+print(13 ** 3)
+print(45 ** 5)
+print(58 / 5)
+print(56 // 8)
+print(63 // 7)
